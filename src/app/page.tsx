6bf6acac7c85ef19c7d8e6b69950d16c8ce09090
myeongjun <midaum79@gmail.com>
2026-05-02 14:29:16 +0900

    Remove unnecessary sections from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import Hero from "@/components/Hero/Hero";
 import Philosophy from "@/components/Philosophy/Philosophy";
-import Portfolio from "@/components/Portfolio/Portfolio";
-import Process from "@/components/Process/Process";
-import Consultation from "@/components/Consultation/Consultation";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Philosophy />
-      <Portfolio />
-      <Process />
-      <Consultation />
     </main>
   );
 }
